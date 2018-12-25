98071ed01161a2892126b16d8234b6c44ff09aa5
Krzychu83 <k.czechowski83@gmail.com>
2018-12-25 22:14:07 +0100

    init project

---
+++ src/main/java/com/czechowski/fromnewsapitoownapi/FromnewsapitoownapiApplication.java
@@ -0,0 +1,14 @@
+package com.czechowski.fromnewsapitoownapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FromnewsapitoownapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FromnewsapitoownapiApplication.class, args);
+    }
+
+}
+
