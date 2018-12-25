d2efa185c3e48a5297e15796086b16d60a2990b8
Krzychu83 <k.czechowski83@gmail.com>
2018-12-25 23:02:23 +0100

    Create TopHeadlinesService, closes #4

---
+++ src/main/java/com/czechowski/fromnewsapitoownapi/input/service/TopHeadlineService.java
@@ -0,0 +1,11 @@
+package com.czechowski.fromnewsapitoownapi.input.service;
+
+import com.czechowski.fromnewsapitoownapi.input.model.TopHeadline;
+
+/**
+ * @author <a href="mailto:k.czechowski83@gmail.com">Krzysztof Czechowski</a>
+ */
+public interface TopHeadlineService {
+
+    TopHeadline findByCountryAndCategory(String country, String category);
+}
