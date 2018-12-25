a974ccf248fa4ed1c6315f90fdcfeb28db28646a
Krzychu83 <k.czechowski83@gmail.com>
2018-12-25 22:58:17 +0100

    Create TopHeadline, closes #3

---
+++ src/main/java/com/czechowski/fromnewsapitoownapi/input/model/TopHeadline.java
@@ -0,0 +1,49 @@
+package com.czechowski.fromnewsapitoownapi.input.model;
+
+import java.util.Arrays;
+
+/**
+ * @author <a href="mailto:k.czechowski83@gmail.com">Krzysztof Czechowski</a>
+ */
+public class TopHeadline {
+
+    String status;
+    String totalResults;
+    Article articles[];
+
+    public String getStatus() {
+        return status;
+    }
+
+    public TopHeadline setStatus(String status) {
+        this.status = status;
+        return this;
+    }
+
+    public String getTotalResults() {
+        return totalResults;
+    }
+
+    public TopHeadline setTotalResults(String totalResults) {
+        this.totalResults = totalResults;
+        return this;
+    }
+
+    public Article[] getArticles() {
+        return articles;
+    }
+
+    public TopHeadline setArticles(Article[] articles) {
+        this.articles = articles;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "TopHeadline{" +
+                "status='" + status + '\'' +
+                ", totalResults='" + totalResults + '\'' +
+                ", articles=" + Arrays.toString(articles) +
+                '}';
+    }
+}
