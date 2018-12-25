eb154d28830136705b4528d4f2b2115181f78eb1
Krzychu83 <k.czechowski83@gmail.com>
2018-12-25 22:52:27 +0100

    Create Article, closes #2

---
+++ src/main/java/com/czechowski/fromnewsapitoownapi/input/model/Article.java
@@ -0,0 +1,102 @@
+package com.czechowski.fromnewsapitoownapi.input.model;
+
+/**
+ * @author <a href="mailto:k.czechowski83@gmail.com">Krzysztof Czechowski</a>
+ */
+public class Article {
+
+    Source source;
+    String author;
+    String title;
+    String description;
+    String url;
+    String urlToImage;
+    String publishedAt;
+    String content;
+
+    public Source getSource() {
+        return source;
+    }
+
+    public Article setSource(Source source) {
+        this.source = source;
+        return this;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public Article setAuthor(String author) {
+        this.author = author;
+        return this;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Article setTitle(String title) {
+        this.title = title;
+        return this;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Article setDescription(String description) {
+        this.description = description;
+        return this;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public Article setUrl(String url) {
+        this.url = url;
+        return this;
+    }
+
+    public String getUrlToImage() {
+        return urlToImage;
+    }
+
+    public Article setUrlToImage(String urlToImage) {
+        this.urlToImage = urlToImage;
+        return this;
+    }
+
+    public String getPublishedAt() {
+        return publishedAt;
+    }
+
+    public Article setPublishedAt(String publishedAt) {
+        this.publishedAt = publishedAt;
+        return this;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public Article setContent(String content) {
+        this.content = content;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "Article{" +
+                "source=" + source +
+                ", author='" + author + '\'' +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", url='" + url + '\'' +
+                ", urlToImage='" + urlToImage + '\'' +
+                ", publishedAt='" + publishedAt + '\'' +
+                ", content='" + content + '\'' +
+                '}';
+    }
+}
