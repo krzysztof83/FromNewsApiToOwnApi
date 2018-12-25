d0c9858a54ba942eb81566c2d86732634ba2aefd
Krzychu83 <k.czechowski83@gmail.com>
2018-12-25 22:50:04 +0100

    Create Source, closes #1

---
+++ src/main/java/com/czechowski/fromnewsapitoownapi/input/model/Source.java
@@ -0,0 +1,28 @@
+package com.czechowski.fromnewsapitoownapi.input.model;
+
+/**
+ * @author <a href="mailto:k.czechowski83@gmail.com">Krzysztof Czechowski</a>
+ */
+public class Source {
+
+    String id;
+    String name;
+
+    public String getId() {
+        return id;
+    }
+
+    public Source setId(String id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Source setName(String name) {
+        this.name = name;
+        return this;
+    }
+}
