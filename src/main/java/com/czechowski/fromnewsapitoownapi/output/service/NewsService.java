e7ebc62335787b43e1d37cf0b24fc26f15504c91
Krzychu83 <k.czechowski83@gmail.com>
2018-12-26 05:48:32 +0100

    Create NewsService, closes #10

---
+++ src/main/java/com/czechowski/fromnewsapitoownapi/output/service/NewsService.java
@@ -0,0 +1,11 @@
+package com.czechowski.fromnewsapitoownapi.output.service;
+
+import com.czechowski.fromnewsapitoownapi.output.model.News;
+
+/**
+ * @author <a href="mailto:k.czechowski83@gmail.com">Krzysztof Czechowski</a>
+ */
+public interface NewsService {
+
+    News findByCountryAndCategory(String country, String category);
+}
