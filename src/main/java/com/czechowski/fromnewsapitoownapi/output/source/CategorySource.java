252bed0a2394385e34af56b14e5e1b0da928e44b
Krzychu83 <k.czechowski83@gmail.com>
2018-12-27 05:08:44 +0100

    Create CategorySource, closes #16

---
+++ src/main/java/com/czechowski/fromnewsapitoownapi/output/source/CategorySource.java
@@ -0,0 +1,22 @@
+package com.czechowski.fromnewsapitoownapi.output.source;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author <a href="mailto:k.czechowski83@gmail.com">Krzysztof Czechowski</a>
+ */
+public class CategorySource {
+
+    private static final Set<String> categorySet = new HashSet<>();
+
+    static {
+
+        String categoryInString = "business entertainment general health science sports technology";
+        String[] categoryTable = categoryInString.split(" ");
+
+        categorySet.addAll(new HashSet<>(Arrays.asList(categoryTable)));
+
+    }
+}
